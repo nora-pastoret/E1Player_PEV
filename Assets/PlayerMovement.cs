78d54736b522b1cc0525e773a5b748e2631a1a60
Lucia-hub11 <lucia.macias@estudiantat.upc.edu>
2024-10-26 12:38:33 +0200

    Move

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,21 +11,23 @@ public class PlayerMovement : MonoBehaviour
     InputControlers _inputs;
     GroundChecker _groundChecker;
 
-    public Rigidbody RigB;
+    //public Rigidbody RigB;
 
     public float Speed = 1;
 
-    public float JumpSpeed = 10;
-    public float AirControl=0.1f; //Perquè hi hagi fricció (que costi moure's a l'aire quan està saltant)
+    //public float JumpSpeed = 10;
+    public float AirControl = 0.1f; //Perquè hi hagi fricció (que costi moure's a l'aire quan està saltant)
+
+    public float TurnSpeed = 1;
 
     private Vector3 _lastVelocity; //Per la gravetat, perquè hi hagi acceleració i variar la velocitat
 
     // Start is called before the first frame update
     void Start()
     {
-       _characterController = GetComponent<CharacterController>();
-       _inputs = GetComponent<InputControlers>();
-       _groundChecker = GetComponentInChildren<GroundChecker>();
+        _characterController = GetComponent<CharacterController>();
+        _inputs = GetComponent<InputControlers>();
+        _groundChecker = GetComponentInChildren<GroundChecker>();
     }
 
     // Update is called once per frame
@@ -34,20 +36,22 @@ public class PlayerMovement : MonoBehaviour
         Move();
     }
 
-    private void Jump(ref Vector3 velocity)
-    {
-        velocity.y = JumpSpeed;
-    }
+    //private void Jump(ref Vector3 velocity)
+    //{
+    //    velocity.y = JumpSpeed;
+    //}
 
-    private bool ShouldJump()
-    {
-        return _inputs.Jump && _groundChecker.Grounded;
-    }
+    //private bool ShouldJump()
+    //{
+    //    return _inputs.Jump && _groundChecker.Grounded;
+    //}
 
     private void Move()
     {
-        Vector3 direction = new Vector3(_inputs.Move.x, 0, _inputs.Move.y);
-        //_characterController.SimpleMove(direction * Speed);
+        var localInput = transform.right * _inputs.Move.x + transform.forward * _inputs.Move.y;
+        Vector3 direction = new Vector3(localInput.x, 0, localInput.z);
+
+        //Vector3 direction = new Vector3(_inputs.Move.x, 0, _inputs.Move.y);
         Vector3 velocity = new Vector3();
 
         float smoothFactor = _groundChecker.Grounded ? 1 : AirControl * Time.deltaTime; //?=if, :=sino --> ESTÀ GROUNDED? SI SI, MOVE NRMAL, SINO FEM AIRCONTROL
@@ -58,8 +62,8 @@ public class PlayerMovement : MonoBehaviour
         velocity.z = Mathf.Lerp(_lastVelocity.z, direction.z * Speed, smoothFactor);
 
         velocity.y = GetGravity();
-        if (ShouldJump())
-            Jump(ref velocity);
+        //if (ShouldJump())
+        //    Jump(ref velocity);
 
 
         _characterController.Move(velocity * Time.deltaTime);
@@ -68,6 +72,8 @@ public class PlayerMovement : MonoBehaviour
         if (direction.magnitude > 0)
         {
             Vector3 target = transform.position + direction;
+            Vector3 current = transform.position + transform.forward;
+            Vector3 look = Vector3.Lerp(current, target, TurnSpeed * Time.deltaTime);
             transform.LookAt(target);
         }
         _lastVelocity = velocity;
